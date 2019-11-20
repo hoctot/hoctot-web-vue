87d6e34da776ddd4288e3ab87d1ba529e1cd0b88
khuongduy1897 <khuongduy1897@gmail.com>
2019-11-21 03:22:59 +0700

    Update Form Validate

--- src/views/CollectionEditor.vue
+++ src/views/CollectionEditor.vue
@@ -1,80 +1,91 @@
 <template>
   <div class="container px-8 mt-5 mx-auto">
     <!-- Two columns -->
-    <div class="flex flex-wrap">
-      <div class="w-full sm:w-3/5">
-        <div>
-          <p class="text-left mb-4">Tiêu đề bộ câu hỏi</p>
-        </div>
-        <div class="editor-wrap">
-          <input
-            v-model="title"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            ref="titleEditor"
-          />
-          <div
-            v-if="isSupportSpeech"
-            :class="{isSpeak: speakType === 'title'}"
-            @click="startRec('title')"
-            class="editor-mic"
-          >
-            <img width="24" src="https://image.flaticon.com/icons/svg/107/107737.svg" />
+    <form id="form"  @submit.prevent="onSubmit">
+      <div class="flex flex-wrap">
+        <div class="w-full sm:w-3/5">
+          <div>
+            <p class="text-left mb-4">Tiêu đề bộ câu hỏi</p>
           </div>
-        </div>
-
-        <div>
-          <p class="text-left mb-4">Giới thiệu</p>
-        </div>
-        <div class="editor-wrap">
-          <textarea
-            v-model="desc"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            ref="descEditor"
-          />
-          <div
-            v-if="isSupportSpeech"
-            :class="{isSpeak: speakType === 'desc'}"
-            @click="startRec('desc')"
-            class="editor-mic"
-          >
-            <img width="24" src="https://image.flaticon.com/icons/svg/107/107737.svg" />
+          <div class="editor-wrap">
+            <input
+              v-model="title"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              ref="titleEditor"
+              minlength="3"
+              maxlength="250"
+              required
+              name="title"
+            />
+            <div
+              v-if="isSupportSpeech"
+              :class="{isSpeak: speakType === 'title'}"
+              @click="startRec('title')"
+              class="editor-mic"
+            >
+              <img width="24" src="https://image.flaticon.com/icons/svg/107/107737.svg" />
+            </div>
           </div>
-        </div>
 
-        <div>
-          <p class="text-left mb-4">URL ảnh (Tuỳ chọn)</p>
-        </div>
-        <div class="editor-wrap">
-          <input
-            v-model="imgUrl"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-          />
-        </div>
+          <div>
+            <p class="text-left mb-4">Giới thiệu</p>
+          </div>
+          <div class="editor-wrap">
+            <textarea
+              v-model="desc"
+              minlength="3"
+              maxlength="250"
+              required
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              ref="descEditor"
+              name="desc"
+            />
+            <div
+              v-if="isSupportSpeech"
+              :class="{isSpeak: speakType === 'desc'}"
+              @click="startRec('desc')"
+              class="editor-mic"
+            >
+              <img width="24" src="https://image.flaticon.com/icons/svg/107/107737.svg" />
+            </div>
+          </div>
 
-        <div class="text-center">
-          <button
-            :class="[
+          <div>
+            <p class="text-left mb-4">URL ảnh (Tuỳ chọn)</p>
+          </div>
+          <div class="editor-wrap">
+            <input
+              v-model="imgUrl"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="url"
+              name="url"
+            />
+          </div>
+
+          <div class="text-center">
+            <button
+              :class="[
               isEdit ? ' ' : 'opacity-50 pointer-events-none shadow-none',
             ]"
-            class="bg-green-500 shadow appearance-none border font-bold rounded py-4 px-8 text-white leading-tight focus:outline-none focus:shadow-outline"
-          >Tạo bộ câu hỏi</button>
+              class="bg-green-500 shadow appearance-none border font-bold rounded py-4 px-8 text-white leading-tight focus:outline-none focus:shadow-outline"
+            >Tạo bộ câu hỏi</button>
+          </div>
         </div>
-      </div>
 
-      <div class="w-full sm:w-2/5">
-        <div class="mx-auto text-center pt-0">
-          <img
-            onmousedown="return false"
-            class="mx-auto my-10 p-5"
-            src="/img/undraw/collections.png"
-            width="275"
-          />
+        <div class="w-full sm:w-2/5">
+          <div class="mx-auto text-center pt-0">
+            <img
+              onmousedown="return false"
+              class="mx-auto my-10 p-5"
+              src="/img/undraw/collections.png"
+              width="275"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -107,6 +118,9 @@ export default {
         const typeEditor = type + 'Editor'
         this.$refs[typeEditor].focus()
       }, 300)
+    },
+    onSubmit(e) {
+      console.log('Submit form', e)
     },
   },
   computed: {
@@ -155,6 +169,9 @@ export default {
 
       this.recognitionInput = recognition
     }
+  },
+  beforeDestroy() {
+    this.recognitionInput.stop()
   },
 }
 </script>
